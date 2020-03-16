534d26a50d7694cbaf2e8bae1a2bafc09a3794df
Rorywork <rorywork95@outlook.com>
2020-03-16 12:38:51 +0000

    Tidied up SCSS

--- server.js
+++ server.js
@@ -12,7 +12,9 @@ const fs = require('fs');
 // configure our express instance with some body-parser settings 
 // including handling JSON data
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 
 // this is where we'll handle our various routes from
 const routes = require('./routes/routes.js')(app, fs);
